9040b4f5c76efad4f6ad26ffcd419bc3a63ef45b
Edwin Lee <leeed2001@gmail.com>
2018-09-28 11:20:11 -0500

    Add demo

---
+++ demos/list_unused_object_name_first_letters.py
@@ -0,0 +1,16 @@
+from pyiddidf.idd.processor import IDDProcessor
+
+file_path = '/home/edwin/Projects/energyplus/repos/4eplus/idd/V8-9-0-Energy+.idd'
+
+idd_processor = IDDProcessor().process_file_given_file_path(file_path)
+object_names = []
+first_letters = set()
+for group in idd_processor.groups:
+    for object in group.objects:
+        object_names.append(object.name)
+        first_letters.add(object.name[0].upper())
+found_letters_set = first_letters
+full_alphabet_set = set([chr(i).upper() for i in range(ord('a'), ord('z') + 1)])
+missing_letters = full_alphabet_set - found_letters_set
+sorted(missing_letters)
+print(missing_letters)
