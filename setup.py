57e39de80726977e186a387c3b151a7835b8dcc6
Edwin Lee <leeed2001@gmail.com>
2023-04-13 21:39:07 -0500

    Update actions, bump version, update setup.py

--- setup.py
+++ setup.py
@@ -19,19 +19,30 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/myoldmopar/py-idd-idf',
-    author='Edwin Lee',
-    author_email='leeed2001@gmail.com',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
-        'License :: Public Domain',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
-    license='UnlicensedForNow',
+    author='Edwin Lee, for NREL, for the United States Department of Energy',
+    license='ModifiedBSD',
     install_requires=[],
     entry_points={
         'console_scripts': ['energyplus_idd_idf=energyplus_iddidf.cli:main_cli']
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Utilities',
+    ],
+    platforms=[
+        'Linux (Tested on Ubuntu)', 'MacOSX', 'Windows'
+    ],
+    keywords=[
+        'energyplus_launch', 'ep_launch',
+        'EnergyPlus', 'eplus', 'Energy+',
+        'Building Simulation', 'Whole Building Energy Simulation',
+        'Heat Transfer', 'HVAC', 'Modeling',
+    ]
 )
